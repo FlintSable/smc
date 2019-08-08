d3992ad861e2c225b4d8e90d409ca0b93c7e8bb9
nicholas <nnlaor@gmail.com>
2019-08-07 18:11:22 -0700

    pushing new file

--- cs21A/005/005-Print_Summary_Statistics.py
+++ cs21A/005/005-Print_Summary_Statistics.py
@@ -1,3 +1,16 @@
-# review data not loaded condition
+""" Assignment five: Print Summary Statistics
+    Author: Nicholas Noochla-or
+    Date: 8/7/2019
 
-# units, rankie
+    Enhancements in this release:
+    -
+    -
+    -
+
+    Review (NN note):
+    - review data not loaded condition
+    - units, rankie
+"""
+
+import math
+
