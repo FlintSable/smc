ed7adbc1a913360360f5593ca07a25b9f81e09cf
Nicholasn <nicholasn@Huxley-007.local>
2021-03-21 13:17:23 -0700

    adding a few base methods to the Card class

--- cs87B/001_Assignment/Cards.py
+++ cs87B/001_Assignment/Cards.py
@@ -6,3 +6,16 @@ class Card:
     def __init__(self, suit=0, rank=2):
         self.suit = suit
         self.rank = rank
+    
+    suit_names = ['Clubs', 'Diamonds', 'Heats', 'Spades']
+    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+    def __str__(self):
+        return '%s of %s' % (Card.rank_names[self.rank_names], Card.suit_names[self.suit])
+
+def main():
+    NewCard = Card()
+    print(NewCard)
+
+if __name__ == "__main__":
+    main()
