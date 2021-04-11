56fd747ce6619983346a8511b0f52093ea5a3677
Nicholasn <nicholasn@Huxley-007.local>
2021-04-10 23:16:49 -0700

    adding exersize file

--- cs87B/005_Assignment/reg.py
+++ cs87B/005_Assignment/reg.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import re
+
+# looking for any reference to http
+regex = r'((http?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)'
+
+data = open("data.txt", "r")
+read_data = data.read()
+websites = re.findall(regex, read_data)
+data.close()
+
+
+print(websites)
